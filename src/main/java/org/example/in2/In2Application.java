081daab2b7f0ebc370e3a62562c66cbe30537e69
DomagojP5 <dp53538@fer.hr>
2025-09-12 14:01:47 +0200

    Initial commit

---
+++ src/main/java/org/example/in2/In2Application.java
@@ -0,0 +1,13 @@
+package org.example.in2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class In2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(In2Application.class, args);
+    }
+
+}
